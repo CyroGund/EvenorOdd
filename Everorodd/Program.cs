af94e2be8ca06d0b53edb79d7a3e70b27a78af7b
Cyro Gund <43975247+CyroGund@users.noreply.github.com>
2019-02-20 20:06:14 -0300

    Add files via upload

---
+++ Everorodd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Everorodd
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int NumeroDigitado;
+            Console.WriteLine("Insira Um Numero:"); // Exibe a Mensagem 
+                   NumeroDigitado = Convert.ToInt32(Console.ReadLine()); //Lê e converte para int o número informado pelo usuário
+            if ( NumeroDigitado % 2 == 0)
+            {
+                //Numero Par
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                //Numero Impar
+                Console.WriteLine("Impar");
+            }
+            Console.ReadKey();
+        }
+    }
+}
